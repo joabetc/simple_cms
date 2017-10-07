f0ee64d4e2f229f48101148ab72c219be4c0d5be
Joabe Costa <joabetc@hotmail.com>
2017-10-07 14:57:01 -0300

    Adding some comments regarding attributes accessors

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,4 +2,14 @@ class AdminUser < ApplicationRecord
 
   # self.table_name = "admin_users"
 
+  # Exemple of pure Ruby Class attributes
+  # attr_accessor :first_name
+  #
+  # def last_name
+  #   @last_name
+  # end
+  #
+  # def last_name=(value)
+  #   @last_name = value
+  # end
 end
